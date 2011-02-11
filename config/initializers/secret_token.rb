cba3e15a8ae4bb2b40a7902bb8d0396a474f66ca
frasse <frasse@sewebbdesign.se>
2011-02-11 10:15:58 +0100

    inint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sewebbdesign2::Application.config.secret_token = '27651dd1c0560c501b1da955b3219fb7407bbd54d72f27949bb53799a0b8ffa938cd9aae1c6241377d1ab371a5c3e34d115c255d60a1a48e20c7a775f6f3709d'
